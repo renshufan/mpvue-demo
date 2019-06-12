7b1455ebb228d0d65488e81cf0514fed7b203b59
renshufan <976590628@qq.com>
2019-06-12 11:39:03 +0800

    添加多个文件

---
+++ src/pages/echarts/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="echarts-wrap">
+    <swiper
+      indicator-dots="indicatorDots"
+      autoplay="true"
+      interval="3000"
+      duration="500"
+      circular="true"
+    >
+      <block v-for="(item,index) in imgUrls" :key="index">
+        <swiper-item>
+          <img :src="item" alt="">
+        </swiper-item>
+      </block>
+    </swiper>
+    <a href="/pages/index/main" class="counter" @click="show=true">路由跳转(首页)</a>
+
+  </div>
+</template>
+
+<script>
+  // import $Message from '/static/dist/base/index'
+  import {$Message} from '../../../static/dist/base/index'
+  export default {
+
+    data () {
+      return {
+        imgUrls: ["/static/image/index/pm1.png", "/static/image/index/pm10.png", "/static/image/index/pm25.png"],
+      }
+    },
+    methods:{
+      handleChange (e) {
+        console.log(e);
+        this.current= e.mp.detail.key;
+      },
+      handleDefault () {
+        $Message({
+          content: '这是一条普通提醒'
+        });
+      },
+      handleSuccess () {
+        $Message({
+          content: '这是一条成功提醒',
+          type: 'success'
+        });
+      },
+      handleWarning () {
+        $Message({
+          content: '这是一条警告提醒',
+          type: 'warning'
+        });
+      },
+      handleError () {
+        $Message({
+          content: '这是一条错误提醒',
+          type: 'error'
+        });
+      },
+      handleDuration () {
+        $Message({
+          content: '我将在 5 秒后消失',
+          duration: 5
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/logs/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="index-width">
+    <map
+      id="map"
+      longitude="113.324520"
+      latitude="23.099994"
+      scale="14"
+      :controls="controls"
+      @controltap="controltap"
+      :markers="markers"
+      @markertap="markertap"
+      :polyline="polyline"
+      @regionchange="regionchange"
+      show-location
+    ></map>
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+import card from '@/components/card'
+
+export default {
+  data(){
+    return {
+      markers: [{
+        iconPath: '/static/image/index/love.png',
+        id: 0,
+        latitude: 23.099994,
+        longitude: 113.324520,
+        width: 50,
+        height: 50
+      }],
+      polyline: [{
+        points: [{
+          longitude: 113.3245211,
+          latitude: 23.10229
+        }, {
+          longitude: 113.324520,
+          latitude: 23.21229
+        }],
+        color: '#FF0000DD',
+        width: 2,
+        dottedLine: true
+      }],
+      controls: [{
+        id: 1,
+        iconPath: '/static/image/index/love.png',
+        position: {
+          left: 0,
+          top: 300 - 50,
+          width: 50,
+          height: 50
+        },
+        clickable: true
+      }]
+    }
+  },
+  components: {
+    card
+  },
+  created () {
+    const logs = (wx.getStorageSync('logs') || [])
+    this.logs = logs.map(log => formatTime(new Date(log)))
+  },
+  methods:{
+    regionchange(e) {
+      console.log(e.type)
+    },
+    markertap(e) {
+      console.log(e.markerId)
+    },
+    controltap(e) {
+      console.log(e.controlId)
+    }
+  }
+}
+</script>
+
+<style scoped>
+   #map,.index-width{
+     width: 100%;
+     height: 100%;
+   }
+</style>
